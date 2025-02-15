2c48b88719171c4a42fcf34d4ce94c1b9f41cd2e
loic.fontaine <lflolo972@gmail.com>
2025-02-15 17:56:09 +0100

    find a way to parseObject and put in a classÃ

--- GithubActivity.java
+++ GithubActivity.java
@@ -29,8 +29,7 @@ public class GithubActivity {
             if (response.statusCode() == 200) {
                 String json = response.body();
                 getEvents(json);
-
-               parseObject(json,0);
+                System.out.println(events.get(0).indexOf("\"id\""));
 
 
             } else {
@@ -41,26 +40,12 @@ public class GithubActivity {
         }
     }
 
-    // Fonction pour extraire une valeur d'une chaîne JSON
-    private static String extractJsonValue(String json, String key) {
-        String searchKey = "\"" + key + "\":";
-        int startIndex = json.indexOf(searchKey);
-        if (startIndex == -1) return "N/A";
+    private static void parseObject(String json, int i) {
+        int index = json.indexOf("\"id\"");
+        System.out.println(index);
 
-        startIndex += searchKey.length();
-
-        // Trouver la fin de la valeur
-        int endIndex;
-        if (json.charAt(startIndex) == '"') { // C'est une chaîne
-            startIndex++;
-            endIndex = json.indexOf('"', startIndex);
-        } else { // C'est un nombre
-            endIndex = json.indexOf(',', startIndex);
-            if (endIndex == -1) endIndex = json.indexOf('}', startIndex); // Dernière valeur du JSON
-        }
-
-        return json.substring(startIndex, endIndex).trim();
     }
+
 
     private static int findMatchingBracket(String json, int startIndex){
         int count = 0;
@@ -95,103 +80,4 @@ public class GithubActivity {
         System.out.println("Nombre d'events trouves : " + events.size());
     }
 
-    public static void parseObject(String json, int indent) {
-        json = json.trim();
-        // Si l'objet commence et se termine par des accolades, on les enlève
-        if (json.startsWith("{") && json.endsWith("}")) {
-            json = json.substring(1, json.length() - 1);
-        }
-
-        int index = 0;
-        while (index < json.length()) {
-            // Passer les espaces ou retours à la ligne
-            while (index < json.length() && Character.isWhitespace(json.charAt(index))) {
-                index++;
-            }
-            if (index >= json.length()) break;
-
-            // Attendre une clé (commençant par ")
-            if (json.charAt(index) == '"') {
-                int keyStart = index + 1;
-                int keyEnd = json.indexOf('"', keyStart);
-                String key = json.substring(keyStart, keyEnd);
-                index = keyEnd + 1;
-
-                // Passer jusqu'au deux-points ':'
-                while (index < json.length() && json.charAt(index) != ':') {
-                    index++;
-                }
-                index++; // Passer le deux-points
-                while (index < json.length() && Character.isWhitespace(json.charAt(index))) {
-                    index++;
-                }
-                if (index >= json.length()) break;
-
-                char c = json.charAt(index);
-                if (c == '{') {
-                    // Valeur est un objet
-                    int objEnd = findMatchingBracket(json, index);
-                    String value = json.substring(index, objEnd + 1);
-                    printIndent(indent);
-                    System.out.println(key + " : ");
-                    parseObject(value, indent + 2);
-                    index = objEnd + 1;
-                } else if (c == '[') {
-                    // Valeur est un tableau
-                    int arrEnd = findMatchingSquareBracket(json, index);
-                    String value = json.substring(index, arrEnd + 1);
-                    printIndent(indent);
-                    System.out.println(key + " : " + value);
-                    index = arrEnd + 1;
-                } else if (c == '"') {
-                    // Valeur est une chaîne de caractères
-                    int valStart = index + 1;
-                    int valEnd = json.indexOf('"', valStart);
-                    String value = json.substring(valStart, valEnd);
-                    printIndent(indent);
-                    System.out.println(key + " : " + value);
-                    index = valEnd + 1;
-                } else {
-                    // Valeur est un nombre, un booléen ou null
-                    int valStart = index;
-                    while (index < json.length() && json.charAt(index) != ',' && json.charAt(index) != '}') {
-                        index++;
-                    }
-                    String value = json.substring(valStart, index).trim();
-                    printIndent(indent);
-                    System.out.println(key + " : " + value);
-                }
-                // Passer la virgule qui sépare les paires, le cas échéant
-                while (index < json.length() && (json.charAt(index) == ',' || Character.isWhitespace(json.charAt(index)))) {
-                    index++;
-                }
-            } else {
-                index++;
-            }
-        }
-    }
-
-    // Affiche des espaces pour l'indentation
-    private static void printIndent(int indent) {
-        for (int i = 0; i < indent; i++) {
-            System.out.print(" ");
-        }
-    }
-
-    // Retourne l'indice de la parenthèse fermante ']' correspondant à l'ouverture à startIndex
-    private static int findMatchingSquareBracket(String json, int startIndex) {
-        int count = 0;
-        for (int i = startIndex; i < json.length(); i++) {
-            char c = json.charAt(i);
-            if (c == '[') {
-                count++;
-            } else if (c == ']') {
-                count--;
-                if (count == 0) {
-                    return i;
-                }
-            }
-        }
-        return -1;
-    }
 }
